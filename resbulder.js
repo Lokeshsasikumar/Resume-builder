c33a3bb73da16e8fb59b31a5e09accc22ad72452
Lokeshsasikumar <145129344+Lokeshsasikumar@users.noreply.github.com>
2023-10-21 18:49:27 +0530

    Add files via upload

---
+++ resbulder.js
@@ -0,0 +1,18 @@
+document.getElementById('resumeForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const address = document.getElementById('address').value;
+    const summary = document.getElementById('summary').value;
+
+    const resumeContent = document.getElementById('resumeContent');
+    resumeContent.innerHTML = `
+        <h2>Resume</h2>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Address:</strong> ${address}</p>
+        <p><strong>Summary:</strong> ${summary}</p>
+    `;
+});
